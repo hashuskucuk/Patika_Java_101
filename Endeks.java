793bf9da4a1007cf368ed1c41d30623396d36a01
hasanhuseyin <hashus2105@gmail.com>
2021-06-27 02:42:03 +0300

    Vücut_-kitle_endeksi

---
+++ Endeks.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Endeks {
+    public static void main(String[] args) {
+        double kilo, boy , sonuc;
+        System.out.println("Lütfen boyunuzu giriniz");
+        Scanner inp = new Scanner(System.in);
+        boy = inp.nextDouble();
+
+        System.out.println("Lütfen  kilonuzu giriniz");
+        kilo =inp.nextDouble();
+
+        sonuc = kilo/(boy*boy);
+        System.out.println("vücut kitle endeksiniz hesaplandý :" +sonuc);
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
