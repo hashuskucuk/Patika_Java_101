d3f8fdc135d31f884a1510dc113ecaa5459033bc
hasanhuseyin <hashus2105@gmail.com>
2021-06-27 22:21:10 +0300

    palindrom_sayılar

---
+++ PalindromSayı.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class PalindromSayı {
+
+
+
+        public static void bul(){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Sayi Giriniz : ");
+            int palindrom = sc.nextInt();
+            int ilkdeger = palindrom;
+            int length = String.valueOf(palindrom).length();
+
+            int kalan,deger=0;
+            for (int i = 0; i<length; i++){
+
+                kalan = palindrom % 10;
+                deger = deger * 10 + kalan;
+                palindrom = palindrom /10;
+            }
+
+            if(deger == ilkdeger){
+                System.out.println(deger + " Palindrom Sayidir.");
+            }else{
+                System.out.println(deger + " Palindrom Sayi Degildir.");
+            }
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            bul();
+        }
+    }
+
