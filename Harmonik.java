ba4f8ba20af3072b25ec199d4e5010620d3d9f0d
hasanhuseyin <hashus2105@gmail.com>
2021-06-27 23:22:42 +0300

    harmonik_seri

---
+++ Harmonik.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Harmonik {
+
+
+
+    
+
+        public static void main(String[] args) {
+            int i, n;
+            int dizi[] = {1, 2, 3, 4, 5};
+            double ort = 0;
+
+            for (i = 0; i < dizi.length; i++) {
+                ort = ort + (1.0 / dizi[i]);
+            }
+            ort = dizi.length / ort;
+            System.out.println("Harmonik Ortalama : " + ort);
+        }
+}
